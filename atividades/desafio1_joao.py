3a7eb10c73a00303b0e33d28f50bc572a0292a10
jovinfo <joaolongo.info@gmail.com>
2025-08-31 12:03:39 -0300

    Limpeza e finalização do desafio 1.

--- atividades/desafio1_joao.py
+++ atividades/desafio1_joao.py
@@ -8,16 +8,16 @@
 
 print('Conversor de temperaturas')
 
-while False: #TODO XXX
+while True:
     print('Escolha a sua opção:')
     escolha = input('Digite 1 para converter de Fahrenheit para Celsius\nDigite 2 para converter de Celsius para Fahrenheit: ')
     if escolha == '1':
-        temp_f = int(input('Insira a sua temperatura em Fahrenheit: '))
+        temp_f = float(input('Insira a sua temperatura em Fahrenheit: '))
         temp_c = (temp_f - 32) * 5/9
         print(f'A temperatura convertida é de {temp_c} °C')
         break
     elif escolha == '2':
-        temp_c = int(input('Insira a sua temperatura em Celsius: '))
+        temp_c = float(input('Insira a sua temperatura em Celsius: '))
         temp_f = (temp_c * 9/5) + 32
         print(f'A temperatura convertida é de {temp_f} °F')
         break
@@ -45,13 +45,6 @@ media = soma/len(temperaturas)
 print(f'A menor temperatura é de: {menor}')
 print(f'A maior temperatura é de: {maior}')
 print(f'A média das temperaturas da lista é de: {media}')
-# print(soma)
-# print(sum(temperaturas))
-# print(maior)
-# print(max(temperaturas))
-# print(menor)
-# print(min(temperaturas))
-
 
 # 3- Você deve criar um programa para controle de estoque de uma loja de decoração, seu programa deve permitir que o usuário realize as seguintes operações:
 # A- Criar o estoque: (Deve ser obrigatoriamente um dicionário)
@@ -62,7 +55,7 @@ print(f'A média das temperaturas da lista é de: {media}')
 # F- Sair do programa.
 # O usuário deve continuar vendo o menu e podendo fazer operações até que selecione uma opção de saída do programa.
 
-str_opcoes = """A - Criar o estoque
+opcoes = """A - Criar o estoque
 B - Adicionar itens utilizando o nome do item e a quantidade.
 C - Consultar um item, utilizando o nome do produto.
 D - Atualizar a quantidade em estoque de um item.
@@ -70,77 +63,60 @@ E - Exibir o estoque final.
 F - Sair do programa.
 """
 
-#TODO HACK criando estoque fora só pra pylance parar de encher
-estoque:dict[str, float] = {}
-#TODO Adicionar elifs em vez de ifs já que as condições são unicas e excludentes
-#TODO Implementar funções para refatorar codigo repetido
+def listar_produtos():
+    print('Os produtos que temos cadastrados são:')
+    for produto in estoque.keys():
+        print(produto)
+
 criado = False
-while False: #TODO XXX
-    escolha = input(f"\nEscolha a sua opção de \'A\' - \'F\'\n{str_opcoes}").lower()
+while True:
+    escolha = input(f"\nEscolha a sua opção de \'A\' - \'F\'\n{opcoes}").lower()
     print('')
     if escolha in ['a','b','c','d','e','f']:
         if escolha == 'a':
-            if not criado:
+            if criado:
+                print('O estoque já existe!')
+            else:
                 estoque = {}
                 criado = True
-                print("Estoque foi criado")
-            else:
-                print("Opção inválida, o estoque já existe")
+                print('O estoque foi criado!')
         
-        if escolha == 'b':
+        elif escolha == 'b':
             if not criado:
                 print("Primeiro você deve escolher a opção 'A' para criar o estoque")
             else:
-                # try:
-                #     x = input()
-                #     estoque.items()
-                # except NameError:
-                #     print("Primeiro você deve escolher a opção 'A' para criar o estoque")
-                # else:
-                #     pass
                 prod_nome = input("Insira o nome do produto a ser criado: ").lower()
                 while prod_nome in estoque.keys():
                     prod_nome = input(f"Esse nome de produto '{prod_nome}' já existe" \
                                     "\nInsira o nome de um #NOVO# produto para ser criado: ").lower()
                 prod_quant = float(input(f"Insira a quantidade de {prod_nome} que tem no estoque: "))
-                #produtos podem ser medidos em unidades de peso, não sao inteiros
                 estoque[prod_nome] = prod_quant
                 print(f'O produto {prod_nome} foi criado com a quantidade {prod_quant}')
             
-        if escolha == 'c':
+        elif escolha == 'c':
             if not criado:
                 print("Primeiro você deve escolher a opção 'A' para criar o estoque")
             else:
-                nomes = estoque.keys()
-                if len(nomes) > 0:
-                    print('Os produtos que temos cadastrados são:')
-                    for produto in nomes:
-                        print(produto)
+                if len(estoque) > 0:
+                    listar_produtos()
                     consulta = input('Digite o nome do produto que deseja consultar: ').lower()
-                    while consulta not in nomes:
-                        print('Os produtos que temos cadastrados são:')
-                        for produto in nomes:
-                            print(produto)
+                    while consulta not in estoque.keys():
+                        listar_produtos()
                         consulta = input(f"Esse nome de produto '{consulta}' não existe!" \
                                     "\nDigite o nome do produto que deseja consultar: ").lower()
                     print(f'O produto \'{consulta}\' tem {estoque[consulta]} no estoque.')
                 else:
                     print("Primeiro você deve escolher a opção 'B' para criar um produto no estoque")
 
-        if escolha == 'd':
+        elif escolha == 'd':
             if not criado:
                 print("Primeiro você deve escolher a opção 'A' para criar o estoque")
             else:
-                nomes = estoque.keys()
-                if len(nomes) > 0:
-                    print('Os produtos que temos cadastrados são:')
-                    for produto in nomes:
-                        print(produto)
+                if len(estoque) > 0:
+                    listar_produtos()
                     alterar = input('Digite o nome do produto que deseja alterar a quantidade: ').lower()
-                    while alterar not in nomes:
-                        print('Os produtos que temos cadastrados são:')
-                        for produto in nomes:
-                            print(produto)
+                    while alterar not in estoque.keys():
+                        listar_produtos()
                         alterar = input(f"Esse nome de produto '{alterar}' não existe!" \
                                     "\nDigite o nome do produto que deseja alterar: ").lower()
                     print(f'O produto \'{alterar}\' tem {estoque[alterar]} no estoque.')
@@ -150,7 +126,7 @@ while False: #TODO XXX
                 else:
                     print("Primeiro você deve escolher a opção 'B' para criar um produto no estoque")
 
-        if escolha == 'e':
+        elif escolha == 'e':
             if not criado:
                 print("Primeiro você deve escolher a opção 'A' para criar o estoque")
             else:
@@ -161,7 +137,7 @@ while False: #TODO XXX
                 else:
                     print("Primeiro você deve escolher a opção 'B' para criar um produto no estoque")
 
-        if escolha == 'f':
+        elif escolha == 'f':
             print('Você escolheu sair, o programa será finalizado!')
             break
     
@@ -175,22 +151,23 @@ while False: #TODO XXX
 # O retorno da função deve ser o valor calculado na conversão.
 
 def converter_temp(unidade_final, valor):
-    if unidade_final == 'c' or unidade_final == 'celsius':
+    if unidade_final == 'c':
         res = (valor - 32) * 5/9
-        return f'Temperatura {valor} °F convertida em Celsius é de {res} °C'
-    elif unidade_final == 'f' or unidade_final == 'fahrenhheit':
+        return res
+    elif unidade_final == 'f':
         res = (valor * 9/5) + 32
-        return f'Temperatura {valor} °C convertida em Fahrenhheit é de {res} °F'
+        return res
     else:
         return None
 
 while True:
     print('Conversor de temperaturas! Escolha a unidade de medida destino da conversão')
-    print('Digite \'C\' ou \'Celsius\' para converter de Fahrenheit para Celsius')
-    final = input('Digite \'F\' ou \'Fahrenhheit\' para converter de Celsius para Fahrenheit: ').lower()
-    temp_inicial = float(input('Insira a sua temperatura inicial: '))
-    if converter_temp(final, temp_inicial) is None:
+    print('Digite \'C\' para converter para Celsius')
+    final = input('Digite \'F\' para converter para Fahrenheit: ').lower()
+    temp_inicial = float(input('Insira a sua temperatura a converter: '))
+    res = converter_temp(final, temp_inicial)
+    if res is None:
         print('Escolha uma opção válida!')
     else:
-        print(converter_temp(final, temp_inicial))
+        print(f'O resultado da conversão é de {res:.2f} °{final.upper()}')
         break
